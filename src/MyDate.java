23cc95a24f7ecd9c4274345aa65b2168c83bd675
Setayesh Ghasemi <setayesh ghasemi>
2021-09-20 16:01:19 +0430

    add method1

--- src/MyDate.java
+++ src/MyDate.java
@@ -33,4 +33,18 @@ public class MyDate {
     public void setYear(int year) {
         this.year = year;
     }
+
+    public boolean isValidDate(int day, int month, int year) {
+        if (month > 0 && month <= 12 && year > 0 && year < 9999) {
+            if (month > 0 && month < 7 && day > 0 && day <= 31)
+                return true;
+            else if (month > 7 && month < 12 && day > 0 && day <= 30)
+                return true;
+            else if (month == 12 && day > 0 && day <= 29)
+                return true;
+            else
+                return false;
+        }
+            return false;
+    }
 }
