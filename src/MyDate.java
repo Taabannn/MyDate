b6ad3f0b810cb795fbdc7a06f52497d42c5db2e0
unknown <tabansoleymani@yahoo.com>
2021-09-20 22:00:18 -0700

    add two last methods: nextDay and toString into MyDate

--- src/MyDate.java
+++ src/MyDate.java
@@ -58,4 +58,77 @@ public class MyDate {
         else
             return -1;
     }
+
+    public void nextDay() {
+        boolean dateChecker;
+        dateChecker = isValidDate(year, month, day + 1);
+        if (dateChecker) {
+            day += 1;
+            return;
+        }
+        dateChecker = isValidDate(year, month + 1, 1);
+        if (dateChecker) {
+            day = 1;
+            month += 1;
+            return;
+        }
+        dateChecker = isValidDate(year + 1, 1, 1);
+        if (dateChecker) {
+            day = 1;
+            month = 1;
+            year += 1;
+            return;
+        }
+        year = 1;
+        month = 1;
+        day = 1;
+        System.out.println("You should check year limits.");
+    }
+
+    @Override
+    public String toString() {
+        String monthName = "";
+        switch (month) {
+            case 1:
+                monthName = "Farvardin";
+                break;
+            case 2:
+                monthName = "Ordibehesht";
+                break;
+            case 3:
+                monthName = "Khordad";
+                break;
+            case 4:
+                monthName = "Tir";
+                break;
+            case 5:
+                monthName = "Mordad";
+                break;
+            case 6:
+                monthName = "Shahrivar";
+                break;
+            case 7:
+                monthName = "Mehr";
+                break;
+            case 8:
+                monthName = "Aban";
+                break;
+            case 9:
+                monthName = "Azar";
+                break;
+            case 10:
+                monthName = "Dey";
+                break;
+            case 11:
+                monthName = "Bahman";
+                break;
+            case 12:
+                monthName = "Esfand";
+                break;
+            default:
+                monthName = "Invalid amount for month";
+                break;
+        }
+        return day + " " + monthName + " " + year;
+    }
 }
